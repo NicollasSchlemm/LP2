034d668d63522b35043d500a4c7529f1dca4b876
NicollasSchlemm <111004545+NicollasSchlemm@users.noreply.github.com>
2022-09-22 17:01:40 -0300

    Add files via upload

---
+++ NIcollasSchlemm/Lp2/Atividade4/Ptriangulo/Ptriangulo/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Ptriangulo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtLadoB = new System.Windows.Forms.TextBox();
+            this.txtLadoA = new System.Windows.Forms.TextBox();
+            this.txtLadoC = new System.Windows.Forms.TextBox();
+            this.Resultado = new System.Windows.Forms.Button();
+            this.Sair = new System.Windows.Forms.Button();
+            this.Limpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(75, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(75, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(75, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "LadoC";
+            // 
+            // txtLadoB
+            // 
+            this.txtLadoB.Location = new System.Drawing.Point(116, 81);
+            this.txtLadoB.Name = "txtLadoB";
+            this.txtLadoB.Size = new System.Drawing.Size(100, 20);
+            this.txtLadoB.TabIndex = 3;
+            this.txtLadoB.TextChanged += new System.EventHandler(this.txtLadoB_TextChanged);
+            // 
+            // txtLadoA
+            // 
+            this.txtLadoA.Location = new System.Drawing.Point(116, 34);
+            this.txtLadoA.Name = "txtLadoA";
+            this.txtLadoA.Size = new System.Drawing.Size(100, 20);
+            this.txtLadoA.TabIndex = 4;
+            this.txtLadoA.TextChanged += new System.EventHandler(this.txtLadoA_TextChanged);
+            // 
+            // txtLadoC
+            // 
+            this.txtLadoC.Location = new System.Drawing.Point(116, 126);
+            this.txtLadoC.Name = "txtLadoC";
+            this.txtLadoC.Size = new System.Drawing.Size(100, 20);
+            this.txtLadoC.TabIndex = 5;
+            this.txtLadoC.TextChanged += new System.EventHandler(this.txtLadoC_TextChanged);
+            // 
+            // Resultado
+            // 
+            this.Resultado.Location = new System.Drawing.Point(128, 168);
+            this.Resultado.Name = "Resultado";
+            this.Resultado.Size = new System.Drawing.Size(75, 23);
+            this.Resultado.TabIndex = 8;
+            this.Resultado.Text = "Resultado";
+            this.Resultado.UseVisualStyleBackColor = true;
+            this.Resultado.Click += new System.EventHandler(this.Resultado_Click);
+            // 
+            // Sair
+            // 
+            this.Sair.Location = new System.Drawing.Point(218, 204);
+            this.Sair.Name = "Sair";
+            this.Sair.Size = new System.Drawing.Size(75, 23);
+            this.Sair.TabIndex = 9;
+            this.Sair.Text = "Sair";
+            this.Sair.UseVisualStyleBackColor = true;
+            this.Sair.Click += new System.EventHandler(this.Sair_Click);
+            // 
+            // Limpar
+            // 
+            this.Limpar.Location = new System.Drawing.Point(35, 204);
+            this.Limpar.Name = "Limpar";
+            this.Limpar.Size = new System.Drawing.Size(75, 23);
+            this.Limpar.TabIndex = 10;
+            this.Limpar.Text = "Limpar";
+            this.Limpar.UseVisualStyleBackColor = true;
+            this.Limpar.Click += new System.EventHandler(this.Limpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(353, 340);
+            this.Controls.Add(this.Limpar);
+            this.Controls.Add(this.Sair);
+            this.Controls.Add(this.Resultado);
+            this.Controls.Add(this.txtLadoC);
+            this.Controls.Add(this.txtLadoA);
+            this.Controls.Add(this.txtLadoB);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtLadoB;
+        private System.Windows.Forms.TextBox txtLadoA;
+        private System.Windows.Forms.TextBox txtLadoC;
+        private System.Windows.Forms.Button Resultado;
+        private System.Windows.Forms.Button Sair;
+        private System.Windows.Forms.Button Limpar;
+    }
+}
+
---
+++ NIcollasSchlemm/Lp2/Atividade4/Ptriangulo/Ptriangulo/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ptriangulo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtLadoA_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtLadoB_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtLadoC_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Limpar_Click(object sender, EventArgs e)
+        {
+            txtLadoA.Clear();
+            txtLadoB.Clear();
+            txtLadoC.Clear();
+            
+        }
+
+        private void Resultado_Click(object sender, EventArgs e)
+        {
+            Double A, B, C;
+             
+
+            if (Double.TryParse(txtLadoA.Text, out A))
+                if (Double.TryParse(txtLadoB.Text, out B))
+                    if (Double.TryParse(txtLadoC.Text, out C))
+                    {
+                       
+                        
+                        if (A == B && A == C)
+                        {
+                            MessageBox.Show("Equilatero");
+                        }
+                        else if (A == B && A != C)
+                        {
+                            MessageBox.Show("Isosceles");
+                        }
+                        else if (A != B && A != C && B != C)
+                        {
+                            MessageBox.Show("escaleno");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Não é um triangulo");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Dados Invalidos");
+                    }
+                else
+                {
+                    MessageBox.Show("Dados Invalidos");
+                }
+            else
+            {
+                MessageBox.Show("Dados Invalidos");
+            }
+
+        }
+
+        private void Sair_Click(object sender, EventArgs e)
+        {
+            Close();
+;        }
+    }
+}
