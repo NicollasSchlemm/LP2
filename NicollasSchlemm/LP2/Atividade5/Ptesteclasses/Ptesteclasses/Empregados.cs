d636dfbd013073bf0837a18a0fd41660c1151bcf
NicollasSchlemm <111004545+NicollasSchlemm@users.noreply.github.com>
2022-10-01 17:14:43 -0300

    Add files via upload

---
+++ NicollasSchlemm/LP2/Atividade5/Ptesteclasses/Ptesteclasses/Empregados.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ptesteclasses
+{
+    abstract class Empregados
+    {
+        private int matricula;//atributo
+        private string nomeEmpregado;
+        private DateTime dataEntradaEmpresa;
+        private char homeOffice;
+
+        public int Matricula //propriedade
+        {
+            get { return matricula; }
+            set { matricula = value; }
+        }
+
+        public string NomeEmpregado
+        {
+            get { return nomeEmpregado; }
+            set { nomeEmpregado = value; }
+        }
+
+        public DateTime DataEntradaEmpresa
+        {
+            get { return dataEntradaEmpresa; }
+            set { dataEntradaEmpresa = value; }
+        }
+
+        public char HomeOffice
+        {
+            get { return homeOffice; }
+            set { homeOffice = value; }
+        }
+
+        public String VerificaHome()
+        {
+            if (homeOffice == 'S')
+                return "Esta em homme office";
+            else
+                return "Não esta em home office";
+
+        }
+
+        public virtual int TempoTrabalho()
+        {
+            TimeSpan span = DateTime.Today.Subtract(DataEntradaEmpresa);
+
+            return (span.Days);
+
+        }
+
+        public abstract double SalarioBruto();
+
+    }
+}
